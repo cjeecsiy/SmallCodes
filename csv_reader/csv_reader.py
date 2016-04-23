bc788c642e071acb0065b0f4941bc3616ac173a7
cjeecsiy <konny198952+github@gmail.com>
2016-04-23 21:01:11 +0900

    少しだけpythonらしく書き直し＆DBアクセスできるように修正

--- csv_reader/csv_reader.py
+++ csv_reader/csv_reader.py
@@ -2,38 +2,53 @@
 
 import sys
 from datetime import datetime
-#import mysql.connector
+import mysql.connector
 
-class CsvReader(object):
+class CsvLoader(object):
+
     def proc(self):
+        try:
+            i = 0
+            config = self.__get_config()
+            cnx = mysql.connector.connect(**config)
+            cur = cnx.cursor(buffered=True)
+            for line in open("test.csv"):
+                itemlist = line[:-1].split(',')
+                (aa, bb, cc, dd, ee, ff) = tuple(itemlist)
+                date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
 
-        i = 0
+                query = (
+                        'insert into hogehoge ('
+                        'aa, bb, cc, dd, ee, ff, gg'
+                        ') values ('
+                        ' \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'%s\'  )' 
+                    ) % ( aa, bb, cc, dd, ee, ff, date)
+                print query
+                cur.execute(query)
+        
+                i += 1
+                if i >= 1000:
+                    cnx.commit()
+                    print i 
+                    i = 0 
+        finally:
+            cnx.commit()
+            cnx.close()
 
-        #cnx = mysql.connector.connect(user='', password='', host='', database='', charset='utf8')
-        #cur = cnx.cursor(bufferd=True)
 
-        for line in open("test.csv"):
-            itemlist = line[:-1].split(',')
-            (aa, bb, cc, dd, ee, ff) = tuple(itemlist)
-            date = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-
-            query = 'insert into hogehoge ('\
-                    'aa, bb, cc, dd, ee, ff, gg'\
-	            ') values ('\
-	            ' %s, %s, %s, %s, %s, %s, %s )'  % ( aa, bb, cc, dd, ee, ff, date)
-            #cur.execute(query)
-            print query
-    
-            i += 1
-            if i >= 1000:
-                # cur.execute(query)
-                # cnx.commit()
-                print i 
-                i = 0 
+    def __get_config(self):
+        return {
+            "database": "mysql_test",
+            "host": "localhost",
+            "user": "root",
+            "password": "",
+            "port": "3306", 
+            "charset": "utf8"
+        }
 
     def __init__(self):
         self.proc()
 
 if __name__ == '__main__':
-    CsvReader()
+    CsvLoader()
 
